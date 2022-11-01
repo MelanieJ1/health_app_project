8100e340868fe4c027281b80271e2819c4729465
Melanie Jayasinghe <melaniej.1@outlook.com>
2022-11-01 06:47:09 +0000

    adding image collection

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -2,3 +2,30 @@ import { dblClick } from "@testing-library/user-event/dist/click"
 
 use health_data
 db.dropDatabase()
+
+db.imageCollection.insertMany{[
+    {
+        "name": "MRI Brain Scan ",
+        "img_url": "https://openi.nlm.nih.gov/imgs/512/212/4837876/PMC4837876_CMJ-128-407-g001.png?keywords=subdural%20effusion,hematomas,subdural%20hematoma"
+    },
+
+    {
+        "name": "Hand X-ray",
+        "img_url": "https://openi.nlm.nih.gov/imgs/512/397/5117157/PMC5117157_10.1177_2324709616677064-fig2.png?keywords=deformity"
+
+    },
+
+    {
+        "name": "Ultrasound - Liver",
+        "img_url": "https://openi.nlm.nih.gov/imgs/512/293/4900112/PMC4900112_gr1.png?keywords=lipomatous%20neoplasm,angiomyolipoma"
+
+    },
+
+    {
+        "name": "Ultrasound - Spleen",
+        "img_url": "https://openi.nlm.nih.gov/imgs/512/268/5034021/PMC5034021_JoU-2016-0027-g002.png?keywords="
+    },
+
+
+
+]}
