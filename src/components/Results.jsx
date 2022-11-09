990981388fb26e012f208e25c364147b48547c87
Melanie Jayasinghe <melaniej.1@outlook.com>
2022-11-09 15:10:49 +0000

    adding test results css

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -7,32 +7,33 @@ const Results = ({result}) => {
       <header className="App-header">
       <>  
       
-    <div>
+    <div className="test-results">
       <br></br>
-      <li>{result.title}</li>
-      <li>{result.test_date}</li>
-      <li>{result.patient_name}</li>
-      <li>{result.white_cell_count}</li>
-      <li>{result.platelets}</li>
-      <li>{result.neutrophils}</li>
-      <li>{result.lymphocytes}</li>
-      <li>{result.monocytes}</li>
-      <li>{result.eosinohils}</li>
-      <li>{result.basophils}</li>
-      <li>{result.erythrocyte_sedimentation_rate}</li>
-      <li>{result.red_blood_cell_count}</li>
-      <li>{result.haemoglobin_level}</li>
-      <li>{result.red_blood_cell_mcv}</li>
-      <li>{result.hemoglobin_mch}</li>
-      <li>{result.glucose_level}</li>
-      <li>{result.total_cholesterol}</li>
-      <li>{result.triglycerids}</li>
-      <li>{result.hdl_cholesterol}</li>
-      <li>{result.ldl_cholesterol}</li>
-      <li>{result.alanine_aminotransferase}</li>
-      <li>{result.albumin}</li>
-      <li>{result.serum_total_protein}</li>
-      <li>{result.alkaline_phosphatase}</li>
+      <h2>{result.title}</h2>
+      <p>Test Date: {result.test_date}
+      <br></br>
+      Patient Name: {result.patient_name}</p>
+      <li>white cell count: {result.white_cell_count}</li>
+      <li>platelets: {result.platelets}</li>
+      <li>neutrophils: {result.neutrophils}</li>
+      <li>lymphocytes: {result.lymphocytes}</li>
+      <li>monocytes: {result.monocytes}</li>
+      <li>eosinohils: {result.eosinohils}</li>
+      <li>basophils: {result.basophils}</li>
+      <li>erythrocyte sedimentation rate: {result.erythrocyte_sedimentation_rate}</li>
+      <li>red blood cell count: {result.red_blood_cell_count}</li>
+      <li>haemoglobin level: {result.haemoglobin_level}</li>
+      <li>red blood cell count mcv: {result.red_blood_cell_mcv}</li>
+      <li>haemoglobin mch: {result.hemoglobin_mch}</li>
+      <li>glucose level: {result.glucose_level}</li>
+      <li>total cholesterol: {result.total_cholesterol}</li>
+      <li>triglycerides: {result.triglycerids}</li>
+      <li>hdl cholesterol: {result.hdl_cholesterol}</li>
+      <li>ldl cholesterol: {result.ldl_cholesterol}</li>
+      <li>alanine aminotransferase: {result.alanine_aminotransferase}</li>
+      <li>albumin: {result.albumin}</li>
+      <li>serum total protein: {result.serum_total_protein}</li>
+      <li>alkaline phosphatase: {result.alkaline_phosphatase}</li>
       <br></br>
       <hr></hr>
       
